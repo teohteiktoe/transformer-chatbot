20597f56bc93361c4c8d641936725b14008830ab
Teoh TeikToe <ttt@mensa.org.sg>
2022-04-12 10:44:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def load_tokenizer_and_model(model="microsoft/DialoGPT-small"):
 def generate_response(tokenizer, model, chat_round, chat_history_ids, text):
     new_input_ids = tokenizer.encode(text + tokenizer.eos_token, return_tensors='pt')
     bot_input_ids = torch.cat([chat_history_ids, new_input_ids], dim=-1) if chat_round > 0 else new_input_ids
-    chat_history_ids = model.generate(bot_input_ids, max_length=1250, pad_token_id=tokenizer.eos_token_id)
+    chat_history_ids = model.generate(bot_input_ids, max_length=100, pad_token_id=tokenizer.eos_token_id)
     r = "Chatbot Reply: {}".format(tokenizer.decode(chat_history_ids[:, bot_input_ids.shape[-1]:][0], skip_special_tokens=True))
     return chat_history_ids, r
 
